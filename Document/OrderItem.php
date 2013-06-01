176251aea1980e957e537fc2a2aa28000e55bc10
Eymen Gunay <eymen@egunay.com>
2013-06-01 06:21:14 +0200

    MongoDB documents added

---
+++ Document/OrderItem.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SalesBundle\Document;
+
+use Sylius\Bundle\SalesBundle\Model\OrderItem as BaseOrderItem;
+
+/**
+ * Base item entity.
+ *
+ * @author Eymen Gunay <eymen@egunay.com>
+ */
+class OrderItem extends BaseOrderItem
+{
+}
